e10be619aebf1081e5fcabe6a4f8479a08d5768d
dektlong <dekel.tankel@gmail.com>
2021-09-23 15:04:30 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"os"
 	"net/http"
 )
 
 var addr = flag.String("addr", ":8080", "addr to bind to")
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.RemoteAddr, r.Method, r.URL.String())
+	fmt.Fprintf(w, "<H2>")
+	
+	fmt.Fprintf(w, r.URL.String())
 
 	fmt.Fprintf(w, "dekt is running adopter-check\n")
+	
+	fmt.Fprintf(w, "</H2>")
+	
+	fmt.Fprintf("My name is: ", os.Getenv("REV"))
 }
 
 func main() {
