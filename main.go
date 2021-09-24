513565975fee7f418cd5fb551b881bbfe36001ab
dektlong <dekel.tankel@gmail.com>
2021-09-24 13:00:59 -0700

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, apiCall)
 	fmt.Fprintf(w, "\n")
 	
-	if (apiCall == "\") {
+	if (apiCall == "") {
         	res = "Please provide a valid API call"
     	} else {
         	req, _ := http.NewRequest("GET", apiCall, nil)
