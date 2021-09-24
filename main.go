6e88be8ba7087dd02f4ebc1921b1ef12d41e6838
dektlong <dekel.tankel@gmail.com>
2021-09-24 12:25:23 -0700

    Update main.go

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ var addr = flag.String("addr", ":8080", "addr to bind to")
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 
-	fmt.Fprintf(w, "hello dekt\n")
-	fmt.Fprintf(w, "url.string:\n")
+	fmt.Fprintf(w, "<H1>Adopter Check API landing page</H1>")
+	fmt.Fprintf(w, "<H2>Executing the following brownfield API: ")
 	fmt.Fprintf(w, r.URL.String())
-	fmt.Fprintf(w, "revision:\n")
+	fmt.Fprintf(w, "</H2>")
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "<H3>Function revision:")
 	fmt.Fprintf(w, os.Getenv("REV"))
+	fmt.Fprintf(w, "<\H3>")
 }
 
 func main() {
