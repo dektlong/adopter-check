01ca0790970ae57f74bf6dc60915faa63c2947d9
dektlong <dekel.tankel@gmail.com>
2021-09-24 14:27:13 -0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-var API_CALL="NONE"
+var API_CALL="datacheck.tanzu.dekt.io/api/adoption-history?adopterID=909-99-9999"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	
