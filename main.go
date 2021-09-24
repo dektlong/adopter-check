bcbee7a15e67cf6370936fede26a0168967ffb5c
dektlong <dekel.tankel@gmail.com>
2021-09-24 12:52:37 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,21 +12,33 @@ import (
 var addr = flag.String("addr", ":8080", "addr to bind to")
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
+	
+	apiCall := r.URL.String()
 
-	fmt.Fprintf(w, "<H1>Adopter Check API landing page</H1>")
-	fmt.Fprintf(w, "<H3>Executing the following brownfield API: ")
-	fmt.Fprintf(w, r.URL.String())
+	fmt.Fprintf(w, "<H1>")
+	fmt.Fprintf(w, "Adopter Check API landing page")
+	fmt.Fprintf(w, "</H1>")
+	
+	fmt.Fprintf(w, "<H3>")
+	fmt.Fprintf(w, "Executing the following brownfield API: ")
+	fmt.Fprintf(w, apiCall)
 	fmt.Fprintf(w, "\n")
 	
-	url := r.URL.String()
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Fprintf(w, string(body))
-	
+	if(apiCall == "\") {
+        	res = "Please provide a valid API call"
+    	} 
+	else 
+	{
+        	req, _ := http.NewRequest("GET", apiCall, nil)
+		res, _ := http.DefaultClient.Do(req)
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		fmt.Fprintf(w, string(body))
+    	}
 	fmt.Fprintf(w, "</H3>")
+	
 	fmt.Fprintf(w, "\n")
 	fmt.Fprintf(w, "\n")
 	fmt.Fprintf(w, "<H4>")
