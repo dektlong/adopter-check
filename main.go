bc562bf2a81b2a9390787f02a76a3fc2a65c3135
dektlong <dekel.tankel@gmail.com>
2021-09-24 12:46:52 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,29 +15,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 
 	fmt.Fprintf(w, "<H1>Adopter Check API landing page</H1>")
-	fmt.Fprintf(w, "<H2>Executing the following brownfield API: ")
+	fmt.Fprintf(w, "<H3>Executing the following brownfield API: ")
+	fmt.Fprintf(w, r.URL.String())
+	fmt.Fprintf(w, "\n")
 	
-	url := "https://reqres.in/api/users"
-
+	url := r.URL.String()
 	req, _ := http.NewRequest("GET", url, nil)
-
 	res, _ := http.DefaultClient.Do(req)
-
 	defer res.Body.Close()
-
 	body, _ := ioutil.ReadAll(res.Body)
-
 	fmt.Fprintf(w, string(body))
 	
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, r.URL.String())
-	fmt.Fprintf(w, "Result: Success")
-	fmt.Fprintf(w, "</H2>")
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, "<H3>")
-	fmt.Fprintf(w, os.Getenv("REV"))
 	fmt.Fprintf(w, "</H3>")
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "<H4>")
+	fmt.Fprintf(w, os.Getenv("REV"))
+	fmt.Fprintf(w, "</H4>")
 }
 
 func main() {
