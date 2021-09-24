a1fee819a6cedbabeb7de6a9c62d22bb158d04e2
dektlong <dekel.tankel@gmail.com>
2021-09-24 12:42:53 -0700

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"io/ioutil"
 )
 
 var addr = flag.String("addr", ":8080", "addr to bind to")
@@ -15,11 +16,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "<H1>Adopter Check API landing page</H1>")
 	fmt.Fprintf(w, "<H2>Executing the following brownfield API: ")
+	
+	url := "https://reqres.in/api/users"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Fprintf(w, string(body))
+	
+	fmt.Fprintf(w, "\n")
 	fmt.Fprintf(w, r.URL.String())
+	fmt.Fprintf(w, "Result: Success")
 	fmt.Fprintf(w, "</H2>")
 	fmt.Fprintf(w, "\n")
 	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, "<H3>Function revision:")
+	fmt.Fprintf(w, "<H3>")
 	fmt.Fprintf(w, os.Getenv("REV"))
 	fmt.Fprintf(w, "</H3>")
 }
