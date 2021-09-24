2e537a2b3690e9b0c2b90d6698bba7e2c2f88883
dektlong <dekel.tankel@gmail.com>
2021-09-24 13:16:20 -0700

    Update main.go

--- main.go
+++ main.go
@@ -14,29 +14,39 @@ var addr = flag.String("addr", ":8080", "addr to bind to")
 var apiCall string
 var res string
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func roothandler(w http.ResponseWriter, r *http.Request) {
 	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 	
-	apiCall = strings.ReplaceAll(r.URL.String(), "/api=", "")
-
 	fmt.Fprintf(w, "<H1>")
-	fmt.Fprintf(w, "Adopter Check API landing page")
+	fmt.Fprintf(w, "Welcome to the Adopter Check function")
 	fmt.Fprintf(w, "</H1>")
+	
+	mt.Fprintf(w, "<H3>")
+	fmt.Fprintf(w, "Please provide a valid 'brownfield API' for execution")
+	
+}
+
+func apihandler(w http.ResponseWriter, r *http.Request) {
+	
+	log.Println(r.RemoteAddr, r.Method, r.URL.String())
+	
+	fmt.Fprintf(w, "<H1>")
+	fmt.Fprintf(w, "Welcome to the Adopter Check function")
+	fmt.Fprintf(w, "</H1>")
+	
+	apiCall = r.URL.String()
 	
 	fmt.Fprintf(w, "<H3>")
 	fmt.Fprintf(w, "Executing the following brownfield API: ")
 	fmt.Fprintf(w, apiCall)
 	fmt.Fprintf(w, "\n")
 	
-	if (apiCall == "") {
-        	res = "Please provide a valid API call"
-    	} else {
-        	req, _ := http.NewRequest("GET", apiCall, nil)
-		res, _ := http.DefaultClient.Do(req)
-		defer res.Body.Close()
-		body, _ := ioutil.ReadAll(res.Body)
-		fmt.Fprintf(w, string(body))
+	req, _ := http.NewRequest("GET", apiCall, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Fprintf(w, string(body))
     	}
 	fmt.Fprintf(w, "</H3>")
 	
@@ -48,7 +58,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	
+	http.HandleFunc("/", roothandler)
+	
+	http.HandleFunc("/api", apihandler)
+	
 
 	log.Printf("listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
