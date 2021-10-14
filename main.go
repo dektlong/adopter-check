5205a9139967ebd4fef95692855369c80bda2b3e
dektlong <dekel.tankel@gmail.com>
2021-10-14 10:52:01 -0700

    Update main.go

--- main.go
+++ main.go
@@ -10,27 +10,37 @@ import (
 
 var API_CALL="http://datacheck.apps.dekt.io/api/adoption-history?adopterID=99-999-9999"
 
+//https://brownfieldapi.example.com/my-brownfield-api
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 	
-	fmt.Fprintf(w, "<H1>Welcome to Adopter Check function</H1>")
-		    
+	fmt.Fprintf(w, "<H1><font color="Teal">")
+	fmt.Fprintf(w, "Welcome to Adopter Check function")
+	fmt.Fprintf(w, "</font></H1>")
+	
 	fmt.Fprintf(w, "<H2>")
-
 	fmt.Fprintf(w, "Brownfield API: ")
+	fmt.Fprintf(w, "<H1><font color="Blue">")
 	fmt.Fprintf(w, API_CALL)
+	fmt.Fprintf(w, "</font>")
 	fmt.Fprintf(w, "<BR><BR>Response: ")
 
 	response, err := http.Get(API_CALL)
 
 	if err != nil {
-    	log.Println(err.Error())
+    		log.Println(err.Error())
+		fmt.Fprintf(w, "<H1><font color="Red">")
 		fmt.Fprintf(w,"Adoption Denied")
+		fmt.Fprintf(w, "</font>")
+		
     } else {
 		responseData, err := ioutil.ReadAll(response.Body)
 		log.Println(string(responseData),err)
+		fmt.Fprintf(w, "<H1><font color="Red">")
 		fmt.Fprintf(w,"Adoption Approved. Get ready to meet your new best friend.")
+		fmt.Fprintf(w, "</Green>")
 	}
 	fmt.Fprintf(w, "</H2>")
 }
