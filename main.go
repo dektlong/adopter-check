7b461daa4a10748b4bdf8b9fd968187677a53fcf
dektlong <dekel.tankel@gmail.com>
2021-10-19 16:35:30 -0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-var API_CALL="http://datacheck.apps.dekt.io/api/adoption-history?adopterID=99-999-9999"
+var API_CALL="brownfield.example.com/api/try-me"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	
